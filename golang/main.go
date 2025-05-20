20a999330d9e44dd22d87bf22f5329fdeefec366
Snigdha Srivatsava <ssrivatsava@umass.edu>
2025-05-20 12:56:45 -0400

    dockerfiles & docker-compose setup

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello from Go!")
+	socketPath := "/tmp/shared_socket"
+
+	conn, err := net.Dial("unix", socketPath)
+	if err != nil {
+		fmt.Println("Dial error:", err)
+		os.Exit(1)
+	}
+
+	message := "Hello from Go!"
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		fmt.Println("Write error:", err)
+	}
+	conn.Close()
 }
--- python/main.py
+++ python/main.py
@@ -1,1 +1,20 @@
-print("Hello from Python!")
+import socket
+import os
+
+SOCKET_FILE = "/tmp/shared_socket"
+
+# Remove socket file if it already exists
+if os.path.exists(SOCKET_FILE):
+    os.remove(SOCKET_FILE)
+
+# Create socket
+server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+server.bind(SOCKET_FILE)
+server.listen(1)
+
+print("Python server listening on Unix socket...")
+
+conn, _ = server.accept()
+msg = conn.recv(1024)
+print(f"Python received: {msg.decode()}")
+conn.close()
